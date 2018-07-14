06e37f2efbaa7e4dc844b3772c12cfd0348f9364
ghost <S0281634@salemstate.edu>
2018-07-14 01:52:17 -0400

    Initial Commit

---
+++ SpellingWasp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpellingWasp
+//
+//  Created by Dakota Kim on 7/14/18.
+//  Copyright © 2018 Dakota Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
