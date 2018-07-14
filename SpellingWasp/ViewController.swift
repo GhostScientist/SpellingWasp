77491c1f2fcd7b3ca39c93f5b29d4d8abe4c79f7
ghost <S0281634@salemstate.edu>
2018-07-14 02:42:58 -0400

    Initial commit

--- SpellingWasp/ViewController.swift
+++ SpellingWasp/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var exampleOutlet: UIButton!
+    @IBOutlet weak var repeatOutlet: UIButton!
+    @IBOutlet weak var solveOutlet: UIButton!
+    @IBOutlet weak var skipOutlet: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var wordField: UILabel!
+    @IBOutlet weak var wordDataLabel: UILabel!
+    
+    var wordsToPresent = [String]()
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        exampleOutlet.clipsToBounds = true
+        exampleOutlet.layer.cornerRadius = 12.0
+        repeatOutlet.clipsToBounds = true
+        repeatOutlet.layer.cornerRadius = 12.0
+        solveOutlet.clipsToBounds = true
+        solveOutlet.layer.cornerRadius = 62.5
+        skipOutlet.clipsToBounds = true
+        skipOutlet.layer.cornerRadius = 62.5
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func exampleTapped(_ sender: UIButton) {
+        
     }
-
-
+    
+    @IBAction func repeatTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func solveTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func skipTapped(_ sender: UIButton) {
+        
+    }
+    
+    func replaceWordWithUnderscore(word: String) -> String {
+        var finalWord = ""
+        for _ in word {
+            finalWord += "_ "
+        }
+        return finalWord
+    }
 }
 
---
+++ SpellingWasp/Word.swift
@@ -0,0 +1,13 @@
+//
+//  Word.swift
+//  SpellingWasp
+//
+//  Created by Dakota Kim on 7/14/18.
+//  Copyright © 2018 Dakota Kim. All rights reserved.
+//
+
+import Foundation
+
+class Word {
+    
+}
